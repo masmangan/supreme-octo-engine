be1d0eb6c0f7944e83280c160b1c1eba95cf3540
Marco Mangan <marco.mangan@gmail.com>
2020-04-17 14:37:28 -0300

    Initial Apache Maven configuration.

---
+++ simple/src/main/java/simple/RioGrandeApp.java
@@ -0,0 +1,16 @@
+package simple;
+
+/**
+ * 
+ * @author marco
+ *
+ */
+public class RioGrandeApp {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Rio Grande App");
+	}
+}
