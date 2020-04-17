b866230369033b0e43b15b9221de34ebb2d328a6
Marco Mangan <marco.mangan@gmail.com>
2020-04-17 17:02:01 -0300

    Formating code.

--- simple/src/main/java/simple/RioGrandeAppLauncher.java
+++ simple/src/main/java/simple/RioGrandeAppLauncher.java
@@ -1,14 +1,21 @@
 package simple;
+
 import javafx.application.Application;
 
 /**
  * 
- * @author marco
+ * @author marco.mangan@gmail.com
+ * 
  * https://openjfx-dev.openjdk.java.narkive.com/aFiw9uqi/error-javafx-runtime-components-are-missing-and-are-required-to-run-this-application
  */
-public final class RioGrandeAppLauncher  {
+public final class RioGrandeAppLauncher {
+
+	/**
+	 * 
+	 */
 	private RioGrandeAppLauncher() {
 	}
+
 	/**
 	 * 
 	 * @param args
