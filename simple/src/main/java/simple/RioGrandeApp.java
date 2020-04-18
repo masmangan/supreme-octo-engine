b070304f8ac929323c32037987be505b4256d1d7
Marco Mangan <marco.mangan@gmail.com>
2020-04-18 09:53:10 -0300

    Header and Footer structure.

--- simple/src/main/java/simple/RioGrandeApp.java
+++ simple/src/main/java/simple/RioGrandeApp.java
@@ -24,46 +24,43 @@ public class RioGrandeApp extends Application {
 	public void start(Stage stage) {
 		stage.setTitle("Rio Grande");
 
-		Label dateLabel = new Label("4000 K");	
-		
-		Label populationLabel = new Label("8/12 (+0,2)");	
+		Label dateLabel = new Label("4000 K");
 
-		Label foodLabel = new Label("+4,8");	
-		
-		Label workLabel = new Label("0 +1,6");	
+		Label populationLabel = new Label("8/12 (+0,2)");
 
-		Label cultureLabel = new Label("0 +1,6");	
+		Label foodLabel = new Label("+4,8");
 
-		Label threatLabel = new Label("0 +1,6");	
+		Label workLabel = new Label("0 +1,6");
+
+		Label cultureLabel = new Label("0 +1,6");
+
+		Label threatLabel = new Label("0 +1,6");
 
 		Button statusButton = new Button("Info");
 		statusButton.setOnAction((ActionEvent e) -> {
-		    System.out.println("Information panel");
+			System.out.println("Information panel");
 		});
-		
+
 		Button settlement = new Button("Settlement");
 		Button techs = new Button("Techs");
 		Button buildings = new Button("Buildings");
 		Button evolution = new Button("Evolution");
 		Button help = new Button("Help");
 		Button menu = new Button("Menu");
-		Label turn = new Label("Turn 1");	
+		Label turn = new Label("Turn 1");
 		Button endTurn = new Button("End Turn");
 		endTurn.setOnAction((ActionEvent e) -> {
-		    System.out.println("End turn...");
-		});		
-		
-	
-		
+			System.out.println("End turn...");
+		});
+
 		BorderPane borderPane = new BorderPane();
-		HBox toolbar = new HBox(dateLabel, populationLabel, foodLabel, workLabel, cultureLabel, threatLabel, statusButton);
+		HBox toolbar = new HBox(dateLabel, populationLabel, foodLabel, workLabel, cultureLabel, threatLabel,
+				statusButton);
 		HBox statusbar = new HBox(settlement, techs, buildings, evolution, help, menu, turn, endTurn);
 		Node appContent = new StackPane();
 		borderPane.setTop(toolbar);
 		borderPane.setCenter(appContent);
 		borderPane.setBottom(statusbar);
-		
-		
 
 		Slider slider = new Slider();
 		slider.setMin(0);
